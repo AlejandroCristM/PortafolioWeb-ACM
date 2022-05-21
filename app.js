c6911b1f13da13dcabf0a170ee786d6457bf8124
Alejandro Cristancho M <alejandro.cristanchom@gmail.com>
2022-05-21 18:07:22 -0500

    feat: Home created and styled

---
+++ app.js
@@ -0,0 +1,14 @@
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            console.log(document.getElementById(button.dataset.id))
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
